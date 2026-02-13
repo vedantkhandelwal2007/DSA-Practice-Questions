a5b3d68266922271d25cda71534bbcdcaeb56985
Vedant Khandelwal <65482974+vedantkhandelwal2007@users.noreply.github.com>
2026-02-13 11:40:33 +0530

    6/2/2026

---
+++ q6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if (n == 0) return 0;
+
+    int j = 0; // index for unique elements
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    for (int i = 0; i <= j; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
